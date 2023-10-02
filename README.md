18ba8da0fb227c30daa19b9a34a899210e3f68a8
khalil bouzoffara <khalil.bouzoffara@ensi-uma.tn>
2023-10-02 12:08:14 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fast-api-app
+
+This is a simple Fast-API app using `VILT` AI model and dockerize it.
---
+++ main.py
@@ -0,0 +1,21 @@
+from model import mode_pipeline
+from fastapi import FastAPI, UploadFile
+from typing import Union
+import io
+from PIL import Image
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {"Hello": "World"}
+
+@app.post('/ask')
+def ask(text: str, image: UploadFile):
+    content = image.file.read()
+    
+    image = Image.open(io.BytesIO(content))
+    #image = Image(image.file)
+    result = mode_pipeline(text, image)
+
+    return {"answer" : result}
